73dd922648ef1abac5ccc72ecc3cea1c47a4462e
playerlove1 <kos83611@gmail.com>
2017-05-08 22:45:20 +0800

    fix test_perceptron
    fix test_perceptron

--- book_machine_learning/decision_bondary.py
+++ book_machine_learning/decision_bondary.py
@@ -19,6 +19,7 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     Z=Z.reshape(xx1.shape)
     
     plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    
     plt.xlim(xx1.min(), xx1.max())
     plt.ylim(xx2.min(), xx2.max())
     
@@ -27,7 +28,7 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     for idx, cl in enumerate(np.unique(y)):
         plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=cmap(idx) ,marker=markers[idx], label=cl)
     
-    plt.xlabel('petal length')
-    plt.ylabel('sepal length')
+    plt.xlabel('petal length [cm]')
+    plt.ylabel('sepal length [cm]')
     plt.legend(loc='upper left')
     plt.show()
--- book_machine_learning/test_perceptron.py
+++ book_machine_learning/test_perceptron.py
@@ -6,30 +6,45 @@ import matplotlib.pyplot as plt
 from perceptron import Perceptron
 from decision_bondary import plot_decision_regions
 
+#讀取資料
 iris = datasets.load_iris()
 # iris=pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
 
 # print(iris.header)
 #取出前100個 鳶尾花的類別標籤
 # y = iris.iloc[0:100, 4].values
-y=iris.target
 
+
+
+# 取前100個
+y=iris.target[0:100]
+
+
+
+#將y=0 的轉為-1  其餘的轉為1
+y = np.where(y == 0, -1, 1)
 #將標籤字串轉為  -1(setosa) or 1(versicolor)
 # y = np.where(y == 'Iris-setosa', -1, 1)
-y = np.where(y == 0, -1, 1)
+
+
+
 
 #取出資料集當中的0,2作為feature
-# X = iris.iloc[0:100,[0,2]].values
 X=iris.data[0:100,[0,2]]
+# X = iris.iloc[0:100,[0,2]].values
 
+#前50個會是同類
 plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+#後50個是另一類
 plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
 
+#顯示類別圖
 plt.xlabel('petal length')
 plt.ylabel('sepal length')
 plt.legend(loc='upper left')
 plt.show()
 
+#Perceptron每輪的錯誤分類數
 ppn=Perceptron(eta=0.1, n_iter=10)
 ppn.fit(X,y)
 plt.plot(range(1,len(ppn.errors_)+1), ppn.errors_, marker='o')
@@ -37,5 +52,7 @@ plt.xlabel('Epochs')
 plt.ylabel("Number of misclassifications")
 plt.show()
 
+
+#決策邊界
 plot_decision_regions(X, y, classifier=ppn)
 

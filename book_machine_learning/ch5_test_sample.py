8b8778dbe32ea40e59ff3552752fc2ae187ea37e
playerlove1 <kos83611@gmail.com>
2017-08-21 10:18:43 +0800

    ch5 example complete
    完成ch5 example

--- book_machine_learning/ch5_test_sample.py
+++ book_machine_learning/ch5_test_sample.py
@@ -6,7 +6,15 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.decomposition import PCA 
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.decomposition import KernelPCA
+from sklearn.datasets import make_moons
+from sklearn.datasets import make_circles
+
+from matplotlib.ticker import FormatStrFormatter
+
 from sample_moudle.visual.plot_decision_regions import plot_decision_regions
+from sample_moudle.algorithm.ch5.rbf_kernel_pca import rbf_kernel_pca
+from sample_moudle.algorithm.ch5.rbf_kernel_pca_project import rbf_kernel_pca_project
 
 uci_wine_url='https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data'
 df_wine = pd.read_csv(uci_wine_url, header=None)
@@ -206,5 +214,148 @@ plot_decision_regions(X_test_lda, y_test, clf=lr)
 plt.xlabel('LD 1')
 plt.ylabel('LD 2')
 plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
+
+#Kernel skill  利用核函數將非線性可分的資料轉換到新的維度空間中 使其成為可以線性分離的
+
+#半月形的dataset
+X,y = make_moons(n_samples=100, random_state=123)
+plt.scatter(X[y==0, 0], X[y==0, 1], color='red', marker='^', alpha=0.5)
+plt.scatter(X[y==1, 0], X[y==1, 1], color='blue', marker='o', alpha=0.5)
+plt.show()
+
+#將半月形dataset用標準PCA呈現   (無法使用線性分類器有效分類)
+scikit_pca = PCA(n_components=2)
+X_spca = scikit_pca.fit_transform(X)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+
+
+ax[0].scatter(X_spca[y==0, 0], X_spca[y==0, 1], color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_spca[y==1, 0], X_spca[y==1, 1], color='blue', marker='o', alpha=0.5)
+
+ax[1].scatter(X_spca[y == 0, 0], np.zeros((50, 1)) + 0.02, color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_spca[y == 1, 0], np.zeros((50, 1)) - 0.02, color='blue', marker='o', alpha=0.5)
+
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+
+plt.tight_layout()
+plt.show()
+
+#使用 RBF KERNEL PCA 將半月型dataset轉換為線性可分
+X_kpca = rbf_kernel_pca(X, gamma=15, n_components=2)
+
+fig, ax = plt.subplots(nrows=1,ncols=2, figsize=(7,3))
+ax[0].scatter(X_kpca[y==0, 0], X_kpca[y==0, 1], color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_kpca[y==1, 0], X_kpca[y==1, 1], color='blue', marker='o', alpha=0.5)
+
+ax[1].scatter(X_kpca[y==0, 0], np.zeros((50,1))+0.02, color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_kpca[y==1, 0], np.zeros((50,1))-0.02, color='blue', marker='o', alpha=0.5)
+
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+ax[0].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+ax[1].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+
+plt.tight_layout()
+plt.show()
+
+#同心圓dataset
+X, y = make_circles(n_samples=1000, random_state=123, noise=0.1, factor=0.2)
+
+plt.scatter(X[y == 0, 0], X[y == 0, 1], color='red', marker='^', alpha=0.5)
+plt.scatter(X[y == 1, 0], X[y == 1, 1], color='blue', marker='o', alpha=0.5)
+
+plt.tight_layout()
+plt.show()
+
+#同心圓dataset用標準PCA呈現   (無法使用線性分類器有效分類)
+scikit_pca = PCA(n_components=2)
+X_spca = scikit_pca.fit_transform(X)
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+
+ax[0].scatter(X_spca[y == 0, 0], X_spca[y == 0, 1], color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_spca[y == 1, 0], X_spca[y == 1, 1], color='blue', marker='o', alpha=0.5)
+
+ax[1].scatter(X_spca[y == 0, 0], np.zeros((500, 1)) + 0.02, color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_spca[y == 1, 0], np.zeros((500, 1)) - 0.02, color='blue', marker='o', alpha=0.5)
+
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+
+plt.tight_layout()
+plt.show()
+
+#使用 RBF KERNEL PCA 將同心圓dataset轉換為線性可分
+X_kpca = rbf_kernel_pca(X, gamma=15, n_components=2)
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+ax[0].scatter(X_kpca[y == 0, 0], X_kpca[y == 0, 1], color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_kpca[y == 1, 0], X_kpca[y == 1, 1], color='blue', marker='o', alpha=0.5)
+
+ax[1].scatter(X_kpca[y == 0, 0], np.zeros((500, 1)) + 0.02, color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_kpca[y == 1, 0], np.zeros((500, 1)) - 0.02, color='blue', marker='o', alpha=0.5)
+
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+
+plt.tight_layout()
+plt.show()
+
+#將新資料投影到轉換的維度上
+
+#半月形dataset
+X, y = make_moons(n_samples=100, random_state=123)
+#回傳核矩陣的特徵值
+alphas, lambdas = rbf_kernel_pca_project(X, gamma=15, n_components=1)
+x_new = X[25]
+print(x_new)
+x_proj = alphas[25] # original projection
+print(x_proj )
+
+#定義投影的函數
+def project_x(x_new, X, gamma, alphas, lambdas):
+    pair_dist = np.array([np.sum((x_new - row)**2) for row in X])
+    k = np.exp(-gamma * pair_dist)
+    return k.dot(alphas / lambdas)
+
+# projection of the "new" datapoint
+x_reproj = project_x(x_new, X, gamma=15, alphas=alphas, lambdas=lambdas)
+print(x_reproj)
+
+#顯示投影 (原投影的點 跟新投影的點重合)
+plt.scatter(alphas[y == 0, 0], np.zeros((50)), color='red', marker='^', alpha=0.5)
+plt.scatter(alphas[y == 1, 0], np.zeros((50)), color='blue', marker='o', alpha=0.5)
+plt.scatter(x_proj, 0, color='black', label='original projection of point X[25]', marker='^', s=100)
+plt.scatter(x_reproj, 0, color='green', label='remapped point X[25]', marker='x', s=500)
+plt.legend(scatterpoints=1)
+
+plt.tight_layout()
+plt.show()
+
+#利用 sklearn的PCA 產生一樣的結果
+X, y = make_moons(n_samples=100, random_state=123)
+scikit_kpca = KernelPCA(n_components=2, kernel='rbf', gamma=15)
+X_skernpca = scikit_kpca.fit_transform(X)
+
+plt.scatter(X_skernpca[y == 0, 0], X_skernpca[y == 0, 1], color='red', marker='^', alpha=0.5)
+plt.scatter(X_skernpca[y == 1, 0], X_skernpca[y == 1, 1], color='blue', marker='o', alpha=0.5)
+
+plt.xlabel('PC1')
+plt.ylabel('PC2')
 plt.tight_layout()
 plt.show()
